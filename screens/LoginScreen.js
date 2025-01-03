b77167c398425ad2d9d562e4350f67a84aec5aee
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2025-01-03 20:03:34 +0200

    Refactored authentication logic to use context for login and signup processes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { login } from "../util/auth";
 import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/context/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authContext = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authContext.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
